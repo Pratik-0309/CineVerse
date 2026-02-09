5c085003937334d5c168d10ee6dc35a6faab4dbb
PratikParate <paratepratik10@gmail.com>
2026-02-09 23:50:40 +0530

    add user model schema

---
+++ backend/model/User.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLenght: 6,
+    },
+    profilePic:{
+        type: String,
+        default: " "
+    },
+    refreshToken: {
+      type: String,
+    }
+},{timestamps: true})
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
