6977a7f66bfca0fbc1eaf5cd529a7a5ec9ce7d7b
PratikParate <paratepratik10@gmail.com>
2026-02-12 14:17:41 +0530

    add user/review/watchlist/favourite API routes endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js';
+import userRouter from './routes/user.route.js';
+import reviewRouter from './routes/review.route.js';
+import watchlistRouter from "./routes/watchlist.route.js"
+import favouriteRouter from './routes/favourite.route.js';
 
 dotenv.config();
 
 const app = express();
 
 app.use(cookieParser());
-app.use(express.json());    
+app.use(express.json());  
 
+connectDB();
+
+app.use("/api/user", userRouter);
+app.use("/api/review", reviewRouter);
+app.use("/api/watchlist", watchlistRouter);
+app.use("/api/favourite",favouriteRouter);
 
 app.get("/",(req,res)=> {
     res.send("Hello World");
