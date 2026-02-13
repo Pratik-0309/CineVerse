345dc6326147d6d33985a250931d4d2dbbf50889
PratikParate <paratepratik10@gmail.com>
2026-02-13 21:10:13 +0530

    add movie controller API routes

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -5,7 +5,17 @@ import {
   popularMovies,
   topRatedMovies,
   upcomingMovies,
-  nowPlayingMovies
+  nowPlayingMovies,
+  searchMovies,
+  searchActors,
+  movieDetails,
+  castAndCrews,
+  MovieTrailer,
+  similarMovies,
+  RecommendedMovies,
+  movieImages,
+  actorDetails,
+  watchProviders,
 } from "../controller/movie.controller.js";
 
 const movieRouter = express.Router();
@@ -16,6 +26,15 @@ movieRouter.get("/popular", popularMovies);
 movieRouter.get("/top-rated", topRatedMovies);
 movieRouter.get("/upcoming", upcomingMovies);
 movieRouter.get("/now-playing", nowPlayingMovies);
-
+movieRouter.get("/search-movies", searchMovies);
+movieRouter.get("/search-actors", searchActors);
+movieRouter.get("/:movieId", movieDetails);
+movieRouter.get("/:movieId/credits", castAndCrews);
+movieRouter.get("/:movieId/videos", MovieTrailer);
+movieRouter.get("/:movieId/similar",similarMovies);
+movieRouter.get("/:movieId/recommendations",RecommendedMovies);
+movieRouter.get("/:movieId/images",movieImages);
+movieRouter.get("/actor/:actorId",actorDetails);
+movieRouter.get("/:movieId/providers",watchProviders);
 
 export default movieRouter;
