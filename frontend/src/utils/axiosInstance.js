219d0332a150585167c6f499e77f2750343311f6
PratikParate <paratepratik10@gmail.com>
2026-02-16 08:48:56 +0530

    create axiosInstance for automatic token refresh

---
+++ frontend/src/utils/axiosInstance.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: "http://localhost:8080",
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    }
+})
+
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        if(!originalRequest){
+            return Promise.reject(error);
+        }
+
+        const status = error.response?.status;
+        const isRefreshCall = originalRequest?.url?.includes(
+            "api/user/refresh-token"
+        )
+
+        if(status === 401 && !originalRequest._retry && !isRefreshCall){
+            originalRequest._retry = true;
+            try {
+                await axios.post(
+                    "http://localhost:8080/api/user/refresh-token",
+                    {},
+                    {withCredentials: true}
+                );
+
+                return axiosInstance(originalRequest)
+            } catch (refreshError) {
+                window.location.href = "/register"
+                return Promise.reject(refreshError);
+            }
+        }
+        return Promise.reject(error);
+    }
+)
+
+export default axiosInstance;
