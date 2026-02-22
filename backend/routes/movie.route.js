e380fc84a2e03d218c21d9dc9a1de9377fdbd379
PratikParate <paratepratik10@gmail.com>
2026-02-22 10:49:59 +0530

    add route for fetching top movies of actors

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -14,6 +14,7 @@ import {
   movieReviews,
   RecommendedMovies,
   movieImages,
+  getActorTopMovies,
   actorDetails,
   watchProviders,
 } from "../controller/movie.controller.js";
@@ -28,6 +29,7 @@ movieRouter.get("/upcoming", upcomingMovies);
 movieRouter.get("/now-playing", nowPlayingMovies);
 movieRouter.get("/search-movies", searchMovies);
 movieRouter.get("/actor/:actorId",actorDetails);
+movieRouter.get("/actor/:actorId/movie",getActorTopMovies)
 movieRouter.get("/:movieId/credits", castAndCrews);
 movieRouter.get("/:movieId/videos", MovieTrailer);
 movieRouter.get("/:movieId/similar",similarMovies);
