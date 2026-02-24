f533772f6b3d5901951ee4d291a1563986df7de4
PratikParate <paratepratik10@gmail.com>
2026-02-24 12:39:59 +0530

    improve axios interceptor to prevent auth retry loop

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,45 +1,53 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080",
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-    }
-})
-
+  baseURL: "http://localhost:8080",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
 
 axiosInstance.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        const originalRequest = error.config;
-        if(!originalRequest){
-            return Promise.reject(error);
-        }
-
-        const status = error.response?.status;
-        const isRefreshCall = originalRequest?.url?.includes(
-            "api/user/refresh-token"
-        )
-
-        if(status === 401 && !originalRequest._retry && !isRefreshCall){
-            originalRequest._retry = true;
-            try {
-                await axios.post(
-                    "http://localhost:8080/api/user/refresh-token",
-                    {},
-                    {withCredentials: true}
-                );
-
-                return axiosInstance(originalRequest)
-            } catch (refreshError) {
-                window.location.href = "/register"
-                return Promise.reject(refreshError);
-            }
-        }
-        return Promise.reject(error);
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (!originalRequest) {
+      return Promise.reject(error);
     }
-)
+
+    const status = error.response?.status;
+    const isRefreshCall = originalRequest?.url?.includes(
+      "/api/user/refresh-token",
+    );
+
+    const isAuthRoute =
+  originalRequest?.url?.includes("/api/user/refresh-token") ||
+  originalRequest?.url?.includes("/api/user/profile") ||
+  originalRequest?.url?.includes("/api/user/logout");
+
+    if (
+      status === 401 &&
+      !originalRequest._retry &&
+      !isRefreshCall &&!isAuthRoute
+    ) {
+      originalRequest._retry = true;
+      try {
+        await axios.post(
+          "http://localhost:8080/api/user/refresh-token",
+          {},
+          { withCredentials: true },
+        );
+
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        window.location.href = "/register";
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
