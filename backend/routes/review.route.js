77fada7954ddf07e34512851df1aefcaedc808c6
PratikParate <paratepratik10@gmail.com>
2026-02-26 18:28:34 +0530

    apply verifyAuth only to protected review routes

--- backend/routes/review.route.js
+++ backend/routes/review.route.js
@@ -1,15 +1,21 @@
-import express from 'express'
-import {addReview, editReview, deleteReview, getMoviesReviews, getUserReviews} from "../controller/review.controller.js"
-import verifyAuth from "../middleware/auth.middleware.js"
+import express from "express";
+import {
+  addReview,
+  editReview,
+  deleteReview,
+  getMoviesReviews,
+  getUserReviews,
+} from "../controller/review.controller.js";
+import verifyAuth from "../middleware/auth.middleware.js";
 
 const reviewRouter = express.Router();
 
-reviewRouter.use(verifyAuth);
-
-reviewRouter.post("/add/:movieId",addReview);
-reviewRouter.put("/edit/:reviewId", editReview);
-reviewRouter.delete("/delete/:reviewId", deleteReview);
 reviewRouter.get("/movie/:movieId", getMoviesReviews);
-reviewRouter.get("/user", getUserReviews);
+
+
+reviewRouter.post("/add/:movieId", verifyAuth, addReview);
+reviewRouter.put("/edit/:reviewId", verifyAuth, editReview);
+reviewRouter.delete("/delete/:reviewId", verifyAuth, deleteReview);
+reviewRouter.get("/user", verifyAuth, getUserReviews);
 
 export default reviewRouter;
